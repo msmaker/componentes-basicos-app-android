6c434b0d3e3530f0397ba525c970576cd71fbf88
Marco Silva <marcosilva.tec@gamil.com>
2021-07-06 10:50:44 -0300

    CheckBox

--- app/src/main/java/com/msmaker/componentesbasicos/MainActivity.java
+++ app/src/main/java/com/msmaker/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText campoNome;
     private EditText campoEmail;
-    private TextView resultado;
+    private TextView textResultado;
+
+    private CheckBox checkVerde, checkBranco, checkVermelho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +23,47 @@ public class MainActivity extends AppCompatActivity {
 
         campoNome = findViewById(R.id.editNome);
         campoEmail = findViewById(R.id.editEmail);
-        resultado = findViewById(R.id.textResultado);
+        textResultado = findViewById(R.id.textResultado);
+
+        //CheckBox
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+
+    public void checkBox() {
+
+        String texto = "";
+
+        if (checkVerde.isChecked()) {
+           // String corSelecionada = checkVerde.getText().toString();
+            //texto = corSelecionada;
+            texto = "Verde selecionado -";
+
+        }
+
+        if (checkBranco.isChecked()) {
+            texto = texto + "Branco selecionado -";
+        }
+
+        if (checkVermelho.isChecked()) {
+            texto = texto + "Vermelho selecionado -";
+        }
+
+        textResultado.setText(texto);
     }
 
     public void enviar(View view) {
+
+        checkBox();
+
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
-        resultado.setText("Nome: " + nome + " \nEmail: " + email);
+        textResultado.setText("Nome: " + nome + " \nEmail: " + email);
+        */
+
     }
 
     public void limpar(View view) {
